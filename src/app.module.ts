1aac269c75db0830dbc17be0771060abc2f605ac
javaidaqib <aqibjavaid.ahmed@systemsltd.com>
2023-10-24 00:27:57 +0500

    add the docker compose file and also set up the prisma schema db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
-    AuthModule
+    AuthModule,
+    PrismaModule
   ],
 })
 export class AppModule {}
